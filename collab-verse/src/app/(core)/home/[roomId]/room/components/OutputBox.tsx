a120a597233c8a7232656aa6d06e1fa5a4a90615
Devgambo <priyanshukumarrai.231cv240@nitk.edu.in>
2025-06-29 01:52:34 +0530

    editor-ui-improve

---
+++ collab-verse/src/app/(core)/home/[roomId]/room/components/OutputBox.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {}
+
+function OutputBox(props: Props) {
+    const {} = props
+
+    return (
+        <div className='bg-black'>
+
+        </div>
+    )
+}
+
+export default OutputBox
